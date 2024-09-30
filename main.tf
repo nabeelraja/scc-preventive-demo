96d35525c2183ab85cc10e16633537842bef224f
Nabeel Raja <nabeel.raja@outlook.com>
2024-09-30 09:58:34 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project = "sec-scc-tst"
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+}
+
+
+resource "google_storage_bucket" "example_bucket" {
+  name          = "example-bucket-1"
+  location      = "EU"
+  force_destroy = true
+
+  project = "sec-scc-tst"
+  versioning {
+    enabled = false
+  }
+  uniform_bucket_level_access = false
+}
